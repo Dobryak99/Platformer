ab2302a905bad378633e42ddf04d3a207b8c2e8c
Никита <Nikita.z99@yandex.ru>
2023-04-27 16:07:50 +0300

    update

--- R.A.I. - The Robot/Input.cpp
+++ R.A.I. - The Robot/Input.cpp
@@ -13,7 +13,7 @@ void Engine::Input()
         {
             if(Keyboard::isKeyPressed(Keyboard::Escape))
             {
-                if(!MainMenu && !GameOver && !LoadNextLevel)                 //don't work if in main menu or game over menu or loading new level
+                if(!MainMenu && !GameOver && !LoadNextLevel && !CreditMenu)                 //don't work if in main menu or game over menu or loading new level
                 {
                     IsPaused = !IsPaused;
                     IsPlaying = !IsPlaying;
