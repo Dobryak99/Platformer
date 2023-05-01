98d8852282584c829aca14dae19c2026288b2b41
Nikita Zhuravlev <102042747+Dobryak99@users.noreply.github.com>
2023-05-01 19:40:31 +0300

    Update RESOURCES.h

--- src/RESOURCES.h
+++ src/RESOURCES.h
@@ -3,42 +3,42 @@
 
 // TEXTURES
 
-#define BACKGROUND_TEXTURE "Resources/textures/game_bg.png"
-#define BACKGROUND_GAME_TEXTURE "Resources/textures/game_bg.png"
-#define BACKGROUND_CAVE_TEXTURE "Resources/textures/background_cave.png"
-#define LOAD_NEW_LEVEL_TEXTURE "Resources/textures/loadlevelbg.png"
-#define TILE_SHEET_TEXTURE "Resources/textures/tile_sheet.png"
-#define PLAYER_STILL_SHEET_TEXTURE "Resources/textures/player_stay_still_sheet.png"
-#define PLAYER_RUN_TEXTURE "Resources/textures/player_run_sheet.png"
-#define ENEMY_TEXTURE "Resources/textures/enemy_sheet.png"
-#define HEART_TEXTURE "Resources/textures/Heart.png"
-#define COLLECTABLE_HEART_TEXTURE "Resources/textures/Heart collectable.png"
-#define COIN_TEXTURE "Resources/textures/coin.png"
-#define TITLE_TEXTURE "Resources/textures/Title.png"
+#define BACKGROUND_TEXTURE "./Resources/textures/game_bg.png"
+#define BACKGROUND_GAME_TEXTURE "./Resources/textures/game_bg.png"
+#define BACKGROUND_CAVE_TEXTURE "./Resources/textures/background_cave.png"
+#define LOAD_NEW_LEVEL_TEXTURE "./Resources/textures/loadlevelbg.png"
+#define TILE_SHEET_TEXTURE "./Resources/textures/tile_sheet.png"
+#define PLAYER_STILL_SHEET_TEXTURE "./Resources/textures/player_stay_still_sheet.png"
+#define PLAYER_RUN_TEXTURE "./Resources/textures/player_run_sheet.png"
+#define ENEMY_TEXTURE "./Resources/textures/enemy_sheet.png"
+#define HEART_TEXTURE "./Resources/textures/Heart.png"
+#define COLLECTABLE_HEART_TEXTURE "./Resources/textures/Heart collectable.png"
+#define COIN_TEXTURE "./Resources/textures/coin.png"
+#define TITLE_TEXTURE "./Resources/textures/Title.png"
 
 // FONT
-#define FONT "Resources/font/PixBob-lite Free.ttf"
+#define FONT "./Resources/font/PixBob-lite Free.ttf"
 
 // LEVELS
-#define LEVEL1 "Resources/map/Level1.txt"
-#define LEVEL2 "Resources/map/Level2.txt"
-#define LEVEL3 "Resources/map/Level3.txt"
-#define LEVEL4 "Resources/map/Level4.txt"
-#define LEVEL5 "Resources/map/Level5.txt"
-#define LEVEL6 "Resources/map/Level6.txt"
-#define LEVEL7 "Resources/map/Level7.txt"
+#define LEVEL1 "./Resources/map/Level1.txt"
+#define LEVEL2 "./Resources/map/Level2.txt"
+#define LEVEL3 "./Resources/map/Level3.txt"
+#define LEVEL4 "./Resources/map/Level4.txt"
+#define LEVEL5 "./Resources/map/Level5.txt"
+#define LEVEL6 "./Resources/map/Level6.txt"
+#define LEVEL7 "./Resources/map/Level7.txt"
 
 //SOUNDS
-#define JUMP_SOUND "Resources/sounds/jump.wav"
-#define COLLECT_COIN_SOUND "Resources/sounds/collect_coin.wav"
-#define COLLECT_HEART_SOUND "Resources/sounds/collect_heart.wav"
-#define DEATH_SOUND "Resources/sounds/GameOver.wav"
-#define ENEMY_DEATH_SOUND "Resources/sounds/monster_death.wav"
-#define NEW_LEVEL_SOUND "Resources/sounds/round_end.wav"
-#define CLICK_SOUND "Resources/sounds/Click_sound.wav"
+#define JUMP_SOUND "./Resources/sounds/jump.wav"
+#define COLLECT_COIN_SOUND "./Resources/sounds/collect_coin.wav"
+#define COLLECT_HEART_SOUND "./Resources/sounds/collect_heart.wav"
+#define DEATH_SOUND "./Resources/sounds/GameOver.wav"
+#define ENEMY_DEATH_SOUND "./Resources/sounds/monster_death.wav"
+#define NEW_LEVEL_SOUND "./Resources/sounds/round_end.wav"
+#define CLICK_SOUND "./Resources/sounds/Click_sound.wav"
 
 //Music
-#define MAIN_MENU_MUSIC "Resources/sounds/main_menu_music.wav"
-#define GAME_MUSIC "Resources/sounds/Space-Music.wav"
+#define MAIN_MENU_MUSIC "./Resources/sounds/main_menu_music.wav"
+#define GAME_MUSIC "./Resources/sounds/Space-Music.wav"
 
 #endif /* RESOURCES_h */
